1766327cf8752433ddb558ef6614de432309ccae
kotaro400 <ktrst0622@gmail.com>
2022-11-02 21:35:09 +0900

    推論規則のテスト

---
+++ src/verifier/test/test_inference.py
@@ -0,0 +1,35 @@
+import pytest
+from typing import List
+from Sequent import Sequent
+from Formula import Formula
+from inference import is_valid_inference
+
+
+@pytest.mark.parametrize(
+    ("assumption_sequent_list", "conclusion_sequent", "expected"),
+    [
+        # {A} |- {C}
+        # -------------
+        # {A, B} |- {C}
+        (
+            [Sequent({Formula("A")}, {Formula("C")})],
+            Sequent({Formula("A"), Formula("B")}, {Formula("C")}),
+            True,
+        ),
+        # {A} |- {C}
+        # -------------
+        # {A} |- {C}
+        (
+            [Sequent({Formula("A")}, {Formula("C")})],
+            Sequent({Formula("A")}, {Formula("C")}),
+            False,
+        ),
+    ],
+)
+def test_LW(
+    assumption_sequent_list: List[Sequent], conclusion_sequent: Sequent, expected: bool
+):
+    assert (
+        is_valid_inference(assumption_sequent_list, conclusion_sequent, "LW")
+        == expected
+    )
