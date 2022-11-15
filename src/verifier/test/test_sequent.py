d7d482e1f685f4de27a59f6e4b917a94a7bddefd
kotaro400 <ktrst0622@gmail.com>
2022-11-15 23:55:00 +0900

    test sequent.is_equal_to

--- src/verifier/test/test_sequent.py
+++ src/verifier/test/test_sequent.py
@@ -14,3 +14,22 @@ from Formula import Formula
 )
 def test_is_axiom(sequent: Sequent, expected: bool):
     assert sequent.is_axiom() == expected
+
+
+@pytest.mark.parametrize(
+    ("sequent1", "sequent2", "expected"),
+    [
+        (
+            Sequent({Formula("A")}, {Formula("A")}),
+            Sequent({Formula("A")}, {Formula("A")}),
+            True,
+        ),
+        (
+            Sequent({Formula("A")}, {Formula("A")}),
+            Sequent({Formula("A")}, {Formula("B")}),
+            False,
+        ),
+    ],
+)
+def test_is_equal_to(sequent1: Sequent, sequent2: Sequent, expected: bool):
+    assert sequent1.is_equal_to(sequent2) == expected
