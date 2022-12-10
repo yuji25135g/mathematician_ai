13f4a50359635433246e6406ab6b8ffe7b5d0377
kotaro400 <ktrst0622@gmail.com>
2022-12-10 17:47:53 +0900

    証明木のテストケース追加

--- src/verifier/test/test_proof_tree.py
+++ src/verifier/test/test_proof_tree.py
@@ -35,11 +35,7 @@ from ProofTree import ProofTree
         # A |- A
         # --------- (R!)
         #   |- A, !A
-        # ------------- (R|1)
-        #   |- A, A | !A
-        # -------------------- (R|2)
-        #   |- A | !A, A | !A
-        # --------------------- (RC)
+        # --------------------- (R|)
         #   |- A | !A
         (
             ProofTree(
@@ -51,6 +47,57 @@ from ProofTree import ProofTree
                             Sequent(set(), {Formula("A"), Formula("!(A)")}),
                             "R!",
                             [Node(Sequent({Formula("A")}, {Formula("A")}))],
+                        )
+                    ],
+                )
+            ),
+            True,
+        ),
+        # -------- (AX)  ---------- (AX)
+        # A |- A          B |- B
+        # --------- (RW) ---------- (LW)
+        # A |- B, A       B, A |- B
+        #               ------------ (R!)
+        #                 A |- B, !B
+        # ------------------------------ (R&)
+        # A |- B, A & !B
+        # ------------------------------ (L!)
+        # A, !(A & !B) |- B
+        # ------------------------------ (R>)
+        # !(A & !B) |- A -> B
+        (
+            ProofTree(
+                Node(
+                    Sequent({Formula("!((A)&(!(B)))")}, {Formula("(A)>(B)")}),
+                    "R>",
+                    [
+                        Node(
+                            Sequent({Formula("A"), Formula("!((A)&(!(B)))")}, {Formula("B")}),
+                            "L!",
+                            [
+                                Node(
+                                    Sequent({Formula("A")}, {Formula("B"), Formula("(A)&(!(B))")}),
+                                    "R&",
+                                    [
+                                        Node(
+                                            Sequent({Formula("A")}, {Formula("B"), Formula("A")}),
+                                            "RW",
+                                            [Node(Sequent({Formula("A")}, {Formula("A")}))],
+                                        ),
+                                        Node(
+                                            Sequent({Formula("A")}, {Formula("B"), Formula("!(B)")}),
+                                            "R!",
+                                            [
+                                                Node(
+                                                    Sequent({Formula("B"), Formula("A")}, {Formula("B")}),
+                                                    "LW",
+                                                    [Node(Sequent({Formula("B")}, {Formula("B")}))],
+                                                )
+                                            ],
+                                        ),
+                                    ],
+                                )
+                            ],
                         )
                     ],
                 )
