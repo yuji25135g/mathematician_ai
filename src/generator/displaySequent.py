fa7bbccdf4a013bc82ea472015ac5a5c64c15480
yuji25135g <yuji25135@gmail.com>
2022-12-12 17:44:38 +0900

    pathの変更

--- src/generator/displaySequent.py
+++ src/generator/displaySequent.py
@@ -1,13 +1,14 @@
-from Formula import Formula
-from Sequent import Sequent
+from ..verifier.Formula import Formula
+from ..verifier.Sequent import Sequent
+
 
 def displaySeq(seq):
-    leftFmls=[]
-    rightFmls=[]
+    leftFmls = []
+    rightFmls = []
     for i in list(seq.left):
         leftFmls.append(i.string_formula)
     for i in list(seq.right):
         rightFmls.append(i.string_formula)
 
-    seq_str = str(leftFmls)+" |- "+str(rightFmls)
+    seq_str = str(leftFmls) + " |- " + str(rightFmls)
     return seq_str
