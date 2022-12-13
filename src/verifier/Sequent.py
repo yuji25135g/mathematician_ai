6a63421d01c5d323d2fe155009d5ca6b4cd32c21
kotaro400 <ktrst0622@gmail.com>
2022-12-13 13:37:35 +0900

    Sequentの文字列化

--- src/verifier/Sequent.py
+++ src/verifier/Sequent.py
@@ -6,6 +6,11 @@ class Sequent:
         self.left = left
         self.right = right
 
+    def __str__(self) -> str:
+        left_string = ", ".join(list(map(lambda f: str(f), self.left)))
+        right_string = ", ".join(list(map(lambda f: str(f), self.right)))
+        return left_string + " |- " + right_string
+
     def is_axiom(self) -> bool:
         return self.left == self.right
 
