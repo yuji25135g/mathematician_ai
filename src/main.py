aa398360e7f67bb49e2b5bd218be654c43f16e7e
yuji25135g <yuji25135@gmail.com>
2022-12-14 13:37:58 +0900

    不要なコメントの削除

--- src/main.py
+++ src/main.py
@@ -10,15 +10,6 @@ from verifier.Sequent import Sequent
 from converter.fml2num import fml2num
 from converter.seq2num import seq2num
 
-# seq1 = Sequent({Formula("C"), Formula("B")}, {Formula("!(A)")})
-
-# print(displaySeq(seq1))
-
-# result = generateUnAction(seq1, [["L!", "!(B)", ""], ["R!", "A", ""]], [0.2, 0.3, 0.5])
-# print(result[1])
-# print(displaySeq(result[0]))
-
-
 from collections import defaultdict
 import numpy as np
 
