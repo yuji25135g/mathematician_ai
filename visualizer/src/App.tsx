3d21c9457d6d8e479132e455bad3c95ffe3eae4c
kotaro400 <ktrst0622@gmail.com>
2022-12-18 17:38:39 +0900

    style調整とディレクトリ整理

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 import Typography from "@mui/material/Typography";
 import Pagination from "@mui/material/Pagination";
-import { CSVReader } from "./CSVUploader/CSVUploader";
-import { StateNetwork } from "./StateNetwork/StateNetwork";
+import { CSVReader } from "./component/CSVUploader/CSVUploader";
+import { StateNetwork } from "./component/StateNetwork/StateNetwork";
+import { StateData } from "./types/StateData";
 
 const App = () => {
   const [step, setStep] = useState(1);
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<StateData[]>([]);
 
   const handleChangeStep = (
     event: React.ChangeEvent<unknown>,
@@ -18,7 +19,7 @@ const App = () => {
 
   return (
     <>
-      <CSVReader setData={setData} />
+      <CSVReader setData={setData} setStep={setStep} />
       <StateNetwork data={data} step={step} />
       <Typography>Step: {step}</Typography>
       <Pagination count={data.length} page={step} onChange={handleChangeStep} />
